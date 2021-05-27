b2c0039f7cdb502e7bb8d5fabca8aedcccd01aa2
waitwalker <waitwalker@163.com>
2021-05-27 09:04:27 +0800

    标准输入输出

--- day01_hello_cpp/main.cpp
+++ day01_hello_cpp/main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
+#include <iostream> /// 标准输入输出流 类似stdio.h
 
+/// std 标准; 使用标准的命名空间
 using namespace std;
 
 int main() {
+    /// cout类似于c语言的printf
+    /// endl类似于c语言的换行符
+    /// cout代表输出设备
+    /// << 代表流向
     cout << " Hello Beautiful World!" << endl;
     cout << "Come on Baby!" << endl;
+    int num = 0;
+    cin >> num;
+    cout << "num == "<< num << endl;
     int b = 0;
     while (b < 20) {
         printf("你好!\n");
