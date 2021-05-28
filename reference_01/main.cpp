6b5083cb53444da50b6b690c1391f1ae672623bd
waitwalker <waitwalker@163.com>
2021-05-28 10:57:52 +0800

    引用作为函数的参数使用

--- reference_01/main.cpp
+++ reference_01/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 using namespace std;
+void my_swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
 
 void test02(){
     int arr[5] = {1,2345,45,6,5};
@@ -16,6 +21,11 @@ void test02(){
 
 int main()
 {
+    int data1 = 10, data2 = 20;
+    cout<<"data1="<<data1<<",data2="<<data2<<endl;
+    my_swap(data1,data2);///这里并不能交换data1和data2的值
+    cout<<"data1="<<data1<<",data2="<<data2<<endl;
+
     test02();
     ///1.引用的基本语法
     int num = 10;
