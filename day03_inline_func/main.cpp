7f5676d82196226882c77492c133a96783296037
waitwalker <waitwalker@163.com>
2021-05-28 15:30:05 +0800

    函数参数默认值

--- day03_inline_func/main.cpp
+++ day03_inline_func/main.cpp
@@ -2,12 +2,23 @@
 
 using namespace std;
 
+int my_add(int a = 20, int b = 0) {
+    return a + b;
+}
+
+void test02(){
+
+}
+
+/// 内联函数的定义 和普通函数相比省去出栈的开销
 inline int my_num(int a, int b) {
     return a * b;
 }
 
 int main()
 {
+    cout<<my_add(10,30)<<endl;
+    return 0;
 
     cout<<my_num(10,20)<<endl;
 
