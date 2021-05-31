51c642daa4f91cdd8d41a0969278c4019a4000f1
waitwalker <waitwalker@163.com>
2021-05-31 17:29:04 +0800

    如果类中有对象成员且想调用对象成员的有参数构造,必须使用初始化列表

--- day_04/day_04/main.cpp
+++ day_04/day_04/main.cpp
@@ -9,8 +9,72 @@
 #include "Data.hpp"
 #include "Person.hpp"
 using namespace std;
+
+
+class A {
+private:
+    int m_a;
+    
+public:
+    A(){
+        cout<<"A无参数构造"<<endl;
+    }
+    A(int a) {
+        m_a = a;
+        cout<<"A有参数构造"<<endl;
+    }
+    ~A(){
+        cout<<"A析构函数"<<endl;
+    }
+};
+
+class B {
+private:
+    int m_b;
+    
+public:
+    B(){
+        cout<<"B无参数构造"<<endl;
+    }
+    B(int b) {
+        m_b = b;
+        cout<<"B有参数构造"<<endl;
+    }
+    ~B(){
+        cout<<"B析构函数"<<endl;
+    }
+};
+
+class DataSource {
+private:
+    A oba;
+    B obb;
+    int data;
+    
+public:
+    DataSource(){
+        cout<<"DataSource无参数构造"<<endl;
+    }
+    
+    DataSource(int a, int b, int c):oba(a),obb(b),data(c){
+        //data = c;
+        cout<<"DataSource有参数构造"<<endl;
+    }
+    
+    ~DataSource(){
+        cout<<"DataSource析构函数"<<endl;
+    }
+};
+
 int main(int argc, const char * argv[]) {
     
+    
+    //DataSource ob1;
+    DataSource ob2(10,20, 30);
+    
+    return 0;
+    
+    /// 初始化列表
     Person person(10,20,30);
     person.showPerson();
     
