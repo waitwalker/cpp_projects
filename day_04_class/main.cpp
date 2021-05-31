b3f9195ce30954e275a91a7c14cf26021464f5de
waitwalker <waitwalker@163.com>
2021-05-31 10:16:48 +0800

    类的属性&方法相关权限

--- day_04_class/main.cpp
+++ day_04_class/main.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int main()
-{
+class Person {
+private:
+    int m_money;//私有数据
+protected:
+    int age;
+public:
+    int num;
+    void desc() {
+        m_money = 100;
+        age = 18;
+        cout<<"This is Person class"<<endl;
+        cout<<"年龄"<<age<<endl;
+    }
+};
+
+int main() {
+    Person lucy;
+    lucy.num = 20;
+    lucy.desc();
     cout << "Hello World!" << endl;
     return 0;
 }
