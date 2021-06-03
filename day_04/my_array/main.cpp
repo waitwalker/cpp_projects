58fabd57de7f4ca1355e996e1e8e88cffac2f3f5
waitwalker <waitwalker@163.com>
2021-06-03 11:17:43 +0800

    自定义数组调试

--- day_04/my_array/main.cpp
+++ day_04/my_array/main.cpp
@@ -6,9 +6,28 @@
 //
 
 #include <iostream>
+#include "MyArray.hpp"
+
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    MyArray arr1;
+    
+    cout<<"容量:"<<arr1.getCapacity()<<endl;
+    cout<<"大小:"<<arr1.getSize()<<endl;
+    
+    MyArray arr2(50);
+    
+    
+    for (int i = 0; i < 20; i++) {
+        arr2.puashBack(i);
+    }
+    
+    cout<<"容量:"<<arr2.getCapacity()<<endl;
+    cout<<"大小:"<<arr2.getSize()<<endl;
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
