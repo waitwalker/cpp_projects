8d42324b4451ecab3a89c568897d607847f68d00
waitwalker <waitwalker@163.com>
2021-06-08 17:17:13 +0800

    同名成员变量处理

--- day05/inherit/main.cpp
+++ day05/inherit/main.cpp
@@ -13,7 +13,12 @@ class Base {
     
     
 public:
+    /// 父类中同名成员变量(属性)
     int num;
+    Base() {
+        this->num = 0;
+        cout<<"父类无参数构造"<<endl;
+    }
     Base(int num) {
         cout<<"父类有参数构造"<<endl;
         this->num = num;
@@ -24,6 +29,7 @@ class Son: public Base {
     
     
 public:
+    /// 子类中同名成员变量(属性)
     int num;
     Son(int num1, int num2):Base(num1) {
         this->num = num2;
