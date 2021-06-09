8516c7f5eb3d6d04d7d16d9ded8c8a25cc01064c
waitwalker <waitwalker@163.com>
2021-06-09 09:14:31 +0800

    多继承项目添加

---
+++ day05ClassInherit/day05ClassInherit/main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  day05ClassInherit
+//
+//  Created by waitwalker on 2021/6/9.
+//
+
+#include <iostream>
+using namespace std;
+
+class Base1 {
+    
+    
+public:
+    int a;
+};
+
+class Base2 {
+    
+    
+public:
+    int b;
+};
+
+class Son: public Base1, public Base2 {
+    
+    
+public:
+    
+};
+
+int main(int argc, const char * argv[]) {
+    
+    Son son;
+    son.a = 100;
+    son.b = 300;
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
