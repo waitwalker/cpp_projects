a3f9cc6b584aeaeb11f092a381847991c1a3e43b
waitwalker <waitwalker@163.com>
2021-06-09 14:36:24 +0800

    父类和子类初始化

--- day06/day06/main.cpp
+++ day06/day06/main.cpp
@@ -7,6 +7,47 @@
 
 #include <iostream>
 
+using namespace std;
+
+class Base {
+    
+    
+public:
+    virtual void sleep() {
+        cout<<"父亲在睡觉"<<endl;
+    }
+};
+
+
+class Son1 {
+public:
+    virtual void sleep() {
+        cout<<"Son1在睡觉"<<endl;
+    }
+};
+
+class Son2 {
+public:
+    virtual void sleep() {
+        cout<<"Son2在睡觉"<<endl;
+    }
+};
+
+class Son3 {
+public:
+    virtual void sleep() {
+        cout<<"Son3在睡觉"<<endl;
+    }
+};
+
+class Son4 {
+public:
+    virtual void sleep() {
+        cout<<"Son4在睡觉"<<endl;
+    }
+};
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
