093877f8bb25df70321667d7c3947603d2ef55fe
waitwalker <waitwalker@163.com>
2021-06-09 14:47:07 +0800

    虚函数的应用:基类指针或引用作为函数参数

--- day06/day06/main.cpp
+++ day06/day06/main.cpp
@@ -13,43 +13,63 @@ class Base {
     
     
 public:
+    Base() {}
     virtual void sleep() {
         cout<<"父亲在睡觉"<<endl;
     }
 };
 
 
-class Son1 {
+class Son1: public Base {
 public:
+    Son1(){}
     virtual void sleep() {
         cout<<"Son1在睡觉"<<endl;
     }
 };
 
-class Son2 {
+class Son2: public Base {
 public:
+    Son2(){}
     virtual void sleep() {
         cout<<"Son2在睡觉"<<endl;
     }
 };
 
-class Son3 {
+class Son3: public Base {
 public:
+    Son3(){}
     virtual void sleep() {
         cout<<"Son3在睡觉"<<endl;
     }
 };
 
-class Son4 {
+class Son4: public Base {
 public:
+    Son4(){}
     virtual void sleep() {
         cout<<"Son4在睡觉"<<endl;
     }
 };
 
+void sleepFun(Base &ob) {
+    ob.sleep();
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    Son1 ob1;
+    Son2 ob2;
+    Son3 ob3;
+    Son4 ob4;
+    
+    Base base;
+    sleepFun(ob1);
+    sleepFun(ob2);
+    sleepFun(ob3);
+    sleepFun(ob4);
+    
     std::cout << "Hello, World!\n";
     return 0;
 }
