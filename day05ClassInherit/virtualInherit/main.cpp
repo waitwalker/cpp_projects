6de517e759189f9f51157e2daefd1f83d16841fb
waitwalker <waitwalker@163.com>
2021-06-09 09:55:15 +0800

    虚继承解决菱形继承问题

---
+++ day05ClassInherit/virtualInherit/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  virtualInherit
+//
+//  Created by waitwalker on 2021/6/9.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Base {
+    
+    
+public:
+    int num;
+};
+
+class Base1: virtual public Base {
+    
+    
+public:
+    int a;
+};
+
+class Base2: virtual public Base {
+    
+    
+public:
+    int a;
+};
+
+class Son: public Base1, public Base2 {
+    
+    
+public:
+    
+};
+
+int main(int argc, const char * argv[]) {
+    
+    Son son;
+    son.num = 100;
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
