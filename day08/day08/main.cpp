3beda963b5929781e8d9c62fdd350f7d152de007
waitwalker <waitwalker@163.com>
2021-06-10 10:47:39 +0800

    函数模板定义

---
+++ day08/day08/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  day08
+//
+//  Created by waitwalker on 2021/6/10.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void swapInt(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+
+void swapChar(char &a, char &b) {
+    char tmp = a;
+    a = b;
+    b = tmp;
+}
+
+/// 函数模板
+template <typename T>
+void mySwap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+int main(int argc, const char * argv[]) {
+    
+    int a = 100;
+    int b = 200;
+    cout<<"a:"<<a<<"\nb:"<<b<<endl;
+    swapInt(a, b);
+    cout<<"a:"<<a<<"\nb:"<<b<<endl;
+    
+    char c = 'c';
+    char d = 'd';
+    cout<<"c:"<<c<<"\nd:"<<d<<endl;
+    swapChar(c, d);
+    cout<<"c:"<<c<<"\nd:"<<d<<endl;
+    
+    mySwap(c, d);
+    cout<<"c:"<<c<<"\nd:"<<d<<endl;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
