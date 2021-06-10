8ef499bc0917f654e8d18a3887810d7b3b6f37b5
waitwalker <waitwalker@163.com>
2021-06-10 16:28:25 +0800

    static_cast用于转换父子类之间的指针或者对象

--- day09/day09/main.cpp
+++ day09/day09/main.cpp
@@ -24,7 +24,19 @@ int main(int argc, const char * argv[]) {
     Base *p1 = new Son;
     
     /// 向下转换
-    Son *p2 = new Base;
+    //Son *p2 = new Base;
+    
+    /// static_cast 用于基本类型转换
+    char ch = 'a';
+    double d = static_cast<double>(ch);
+    
+    /// static 用于类指针之间的转换
+    Son *p2;
+    p2 = static_cast<Son *>(p1);
+    delete p1;
+    
+    Base *p3 = static_cast<Base *>(new Son);
+    delete p3;
     std::cout << "Hello, World!\n";
     return 0;
 }
