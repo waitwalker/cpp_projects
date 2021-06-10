08263b39f402880d004d26e37d18e3fd4e737e4a
waitwalker <waitwalker@163.com>
2021-06-10 11:40:14 +0800

    函数模板遍历数组

--- day08/day08/main.cpp
+++ day08/day08/main.cpp
@@ -31,8 +31,7 @@ void mySwap(T &a, T &b) {
     b = tmp;
 }
 
-int main(int argc, const char * argv[]) {
-    
+void test01() {
     int a = 100;
     int b = 200;
     cout<<"a:"<<a<<"\nb:"<<b<<endl;
@@ -47,6 +46,28 @@ int main(int argc, const char * argv[]) {
     
     mySwap<char>(c, d);
     cout<<"c:"<<c<<"\nd:"<<d<<endl;
+}
+
+template <typename T1>
+void myPrintArray(T1 &t1, int t2) {
+    for (int i = 0; i < t2; i++) {
+        cout<<t1[i]<<"  "<<endl;
+        cout<<endl;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    
+    //test01();
+    char str[] = "hello template";
+    int strLength = (int)strlen(str);
+    
+    int arr[] = {24,3,6,7,36,8};
+    int arrLength = sizeof(arr) / sizeof(arr[0]);
+    
+    
+    myPrintArray(str, strLength);
+    myPrintArray(arr, arrLength);
     std::cout << "Hello, World!\n";
     return 0;
 }
