f6596a355507464d762b6c68ce0dd0aa8adf270e
waitwalker <waitwalker@163.com>
2021-06-10 16:21:33 +0800

    向上&向下转换演示

--- day09/day09/main.cpp
+++ day09/day09/main.cpp
@@ -6,9 +6,25 @@
 //
 
 #include <iostream>
+using namespace std;
+class Base {
+    
+    
+public:
+    int num;
+};
+
+class Son: public Base {
+public:
+    
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    /// 向上转换
+    Base *p1 = new Son;
+    
+    /// 向下转换
+    Son *p2 = new Base;
     std::cout << "Hello, World!\n";
     return 0;
 }
