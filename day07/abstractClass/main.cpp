f021b1d4a891fb414db59a129d151197d90612c9
waitwalker <waitwalker@163.com>
2021-06-10 09:27:26 +0800

    纯虚析构函数的实现步骤

--- day07/abstractClass/main.cpp
+++ day07/abstractClass/main.cpp
@@ -11,10 +11,16 @@ using namespace std;
 
 class AbstractBase {
     
-    
 public:
+    /// 第一步 virtual修饰 变成虚析构函数
+    /// 第二步 =0 变成纯虚析构函数
     virtual ~AbstractBase() = 0;
 };
+
+/// 第三步 在外部实现 
+AbstractBase::~AbstractBase() {
+    
+}
 
 int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
