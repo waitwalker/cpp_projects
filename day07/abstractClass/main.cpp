6457340fdf638b6322db567d19d2cc9e1b4a8a9e
waitwalker <waitwalker@163.com>
2021-06-10 09:24:06 +0800

    纯虚析构函数除了等于0还需要实现

---
+++ day07/abstractClass/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  abstractClass
+//
+//  Created by waitwalker on 2021/6/10.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class AbstractBase {
+    
+    
+public:
+    virtual ~AbstractBase() = 0;
+};
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
