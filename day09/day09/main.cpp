3a92e5b57c72fb13d6552ce4d833f0d5d1e043a9
waitwalker <waitwalker@163.com>
2021-06-10 16:18:42 +0800

    创建day09工程

---
+++ day09/day09/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  day09
+//
+//  Created by waitwalker on 2021/6/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
