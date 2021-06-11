787c4d7aa75511b4d816c4aadd1579bb02cc9bc5
waitwalker <waitwalker@163.com>
2021-06-11 11:23:10 +0800

    vector 容器

---
+++ day11/day11/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  day11
+//
+//  Created by waitwalker on 2021/6/11.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    // 单端动态数组vector 类模板
+    vector<int> v;//v就是一个具体的容器
+    // 尾插法
+    v.push_back(100);
+    v.push_back(200);
+    v.push_back(300);
+    v.push_back(400);
+    
+    // 访问数据
+    
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
