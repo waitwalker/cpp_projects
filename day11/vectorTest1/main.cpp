8c2fe98b00b304a8b7b4c74b665036dbf65f8e2f
waitwalker <waitwalker@163.com>
2021-06-11 14:17:42 +0800

    容器嵌套容器

--- day11/vectorTest1/main.cpp
+++ day11/vectorTest1/main.cpp
@@ -6,8 +6,8 @@
 //
 
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Person {
@@ -26,6 +26,7 @@ void myPrint(Person &per) {
     cout<<"name = "<<per.name<<",age = "<<per.age<<endl;
 }
 
+// 容器存放自定义数据类型
 void test1() {
     Person ob1("张1",11);
     Person ob2("张2",12);
@@ -41,8 +42,40 @@ void test1() {
     for_each(vec.begin(), vec.end(), myPrint);
 }
 
+// 容器嵌套
+void test2() {
+    vector<int> v1;
+    vector<int> v2;
+    vector<int> v3;
+    
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    
+    v2.push_back(100);
+    v2.push_back(200);
+    v2.push_back(300);
+    
+    v3.push_back(1000);
+    v3.push_back(2000);
+    v3.push_back(3000);
+    
+    vector<vector<int>> v;
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+    
+    for (vector<vector<int>>::iterator it = v.begin(); it != v.end(); it++) {
+        for (vector<int>::iterator tmpIt = (*it).begin(); (*it).begin() != (*it).end(); tmpIt++) {
+            cout<<*tmpIt<<endl;
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    test1();
+    //test1();
+    
+    test2();
     std::cout << "Hello, World!\n";
     return 0;
 }
