50e44d9e7ace9f26c025951e9f1fe563d44d23e7
waitwalker <waitwalker@163.com>
2021-06-11 08:58:07 +0800

    异常抛出&捕获

--- day10/day10/main.cpp
+++ day10/day10/main.cpp
@@ -16,14 +16,36 @@ int myDiv(int a, int b) {
     return a/b;
 }
 
-int main(int argc, const char * argv[]) {
-    
+void test01() {
     int ret = myDiv(10, -10);
     if (ret == -1) {
         cout<<"程序异常了"<<endl;
     } else {
         cout<<"程序正常"<<endl;
     }
+}
+
+int myDiv02(int a, int b) {
+    if (b == 0) {
+        throw 0;
+    }
+    return a/b;
+}
+
+void test02() {
+    try {
+        int ret = myDiv02(10, -10);
+        cout<<"ret = "<<ret<<endl;
+    } catch (int e) {
+        cout<<"exception = " <<e<<endl;
+    } catch (...) {
+        cout<<"捕获到其他异常"<<endl;
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    //test01();
+    test02();
     
     std::cout << "Hello, World!\n";
     return 0;
