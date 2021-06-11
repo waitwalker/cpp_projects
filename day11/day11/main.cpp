5f9e0904771ccb2edf70f21013f682f790cbfb16
waitwalker <waitwalker@163.com>
2021-06-11 13:37:07 +0800

    vector 容器的遍历

--- day11/day11/main.cpp
+++ day11/day11/main.cpp
@@ -21,7 +21,21 @@ int main(int argc, const char * argv[]) {
     v.push_back(400);
     
     // 访问数据
+    // 定义一个迭代器 存储v的起始迭代器 迭代器依赖于容器的
+    vector<int>::iterator beginIt = v.begin();
     
+    // 定义一个迭代器 存储v的结束迭代器
+    vector<int>::iterator endId = v.end();
+    // for循环遍历1
+    for (; beginIt != endId;beginIt++) {
+        // 对迭代器取* 代表的是 容器的元素
+        cout<<*beginIt<<endl;
+    }
+    
+    // for循环遍历2
+    for (vector<int>::iterator it= v.begin(); it != v.end(); it++) {
+        cout<<"元素:"<<*it<<endl;
+    }
     
     std::cout << "Hello, World!\n";
     return 0;
