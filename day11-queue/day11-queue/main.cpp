54e588e31f5597ed8440aac7369f65e54d7b052d
waitwalker <waitwalker@163.com>
2021-06-16 10:24:06 +0800

    queue出队操作

--- day11-queue/day11-queue/main.cpp
+++ day11-queue/day11-queue/main.cpp
@@ -6,9 +6,25 @@
 //
 
 #include <iostream>
+#include <queue>
+using namespace std;
+
+void test1() {
+    queue<int> que;
+    que.push(10);
+    que.push(20);
+    que.push(30);
+    
+    cout<<"尾元素:"<<que.back()<<endl;
+    cout<<"首元素:"<<que.front()<<endl;
+    
+    while (!que.empty()) {
+        cout<<"首元素:"<<que.front()<<endl;
+        // 出队操作
+        que.pop();
+    }}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    test1();
     return 0;
 }
