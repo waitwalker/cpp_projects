b67e895549af098c7fb24f0a24ce15d41663a607
waitwalker <waitwalker@163.com>
2021-06-16 11:02:50 +0800

    链表的头插法&尾插法&遍历

--- day11-list/day11-list/main.cpp
+++ day11-list/day11-list/main.cpp
@@ -6,9 +6,25 @@
 //
 
 #include <iostream>
+#include <list>
+
+using namespace std;
+
+void printList(list<int> &L) {
+    for (list<int>::iterator it = L.begin(); it != L.end(); it++) {
+        cout<<"元素:"<<*it<<endl;
+    }
+}
+
+void test1() {
+    list<int> L;
+    L.push_back(10);
+    L.push_back(20);
+    L.push_front(9);
+    printList(L);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    test1();
     return 0;
 }
