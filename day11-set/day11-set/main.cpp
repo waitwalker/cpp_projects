8746d7f5bdb0dc1f06cc168991c89e950cc385da
waitwalker <waitwalker@163.com>
2021-06-16 17:02:10 +0800

    set容器插入元素&遍历

--- day11-set/day11-set/main.cpp
+++ day11-set/day11-set/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+#include <string.h>
+#include <set>
+
+
+using namespace std;
+
+
+
+void test1() {
+    set<int> s;
+    s.insert(10);
+    s.insert(30);
+    s.insert(1);
+    s.insert(8);
+    for_each(s.begin(), s.end(), [](int value){
+        cout<<"value:"<<value<<endl;
+    });
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    test1();
     return 0;
 }
