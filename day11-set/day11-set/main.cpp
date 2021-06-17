e8ba96aa386fb7197060a61f44f33fbc1c82658f
waitwalker <waitwalker@163.com>
2021-06-17 08:51:11 +0800

    set迭代器的查找方法

--- day11-set/day11-set/main.cpp
+++ day11-set/day11-set/main.cpp
@@ -23,13 +23,26 @@ void test1() {
     for_each(s.begin(), s.end(), [](int value){
         cout<<"value:"<<value<<endl;
     });
-    set<int>::iterator it = s.begin();
+    set<int>::const_iterator it = s.begin();
     //*it = 100;
+    // 删除元素 会把所有相同元素都删除
     s.erase(10);
     
     for_each(s.begin(), s.end(), [](int value){
         cout<<"value:"<<value<<endl;
     });
+    
+    set<int>::const_iterator ret;
+    set<int>::const_iterator iter = s.find(8);
+    if (ret == s.find(8)) {
+        cout<<"找到的值:"<<*(ret)<<endl;
+        
+    } else {
+        cout<<"没有找到"<<endl;
+    }
+    cout<<"找到的值:"<<*(iter)<<endl;
+    //cout<<"找到的值:"<<*(ret)<<endl;
+    
 }
 
 int main(int argc, const char * argv[]) {
