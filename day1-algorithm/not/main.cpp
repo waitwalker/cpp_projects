4a78b60ef125da72e6ffbde52cfcb203c70dad8a
waitwalker <waitwalker@163.com>
2021-06-17 16:11:50 +0800

    排序规则,普通函数可以实现.仿函数重写小括号也能实现

--- day1-algorithm/not/main.cpp
+++ day1-algorithm/not/main.cpp
@@ -32,9 +32,37 @@ void test1() {
     cout<<*it<<endl;
 }
 
+class MyGreater2 {
+    
+    
+public:
+    bool operator()(int v1, int v2) {
+        return v1 > v2;
+    }
+};
+
+void test2() {
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    for_each(v.begin(), v.end(), [](int value){
+        cout<<value<<" ";
+    });
+    cout<<endl;
+    sort(v.begin(), v.end(), MyGreater2());
+    for_each(v.begin(), v.end(), [](int value){
+        cout<<value<<" ";
+    });
+    cout<<endl;
+}
+
 int main(int argc, const char * argv[]) {
     
-    test1();
+    //test1();
+    test2();
     
     return 0;
 }
