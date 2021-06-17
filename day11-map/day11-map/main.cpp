90014f36858a2371be4c5d806504292cee196656
waitwalker <waitwalker@163.com>
2021-06-17 11:24:37 +0800

    map容器初始化&插入数据的方式

---
+++ day11-map/day11-map/main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  day11-map
+//
+//  Created by waitwalker on 2021/6/17.
+//
+
+#include <iostream>
+#include <string.h>
+#include <map>
+using namespace std;
+
+void test1() {
+    map<string, int> m;
+    // 第一种插入方式 不推荐
+    m.insert(pair<string, int>("key1",123));
+    
+    // 第二种插入方式 推荐
+    m.insert(make_pair("key2", 1234));
+    
+    // 第三种插入方式 不推荐
+    m.insert(map<string,int>::value_type("key3",12345));
+    
+    // 第四种插入方式 读推荐 写不推荐
+    m["key4"] = 123456;
+}
+
+
+int main(int argc, const char * argv[]) {
+    test1();
+    return 0;
+}
