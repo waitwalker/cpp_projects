367f868719e67ce02ad6b73cfb032500f55003d5
waitwalker <waitwalker@163.com>
2021-06-18 09:23:04 +0800

    find_if按条件查找,通过普通函数实现

--- day1-algorithm/transform/main.cpp
+++ day1-algorithm/transform/main.cpp
@@ -76,16 +76,38 @@ void test3() {
     v.push_back(Person("张1", 13));
     v.push_back(Person("张2", 14));
     v.push_back(Person("张5", 15));
-    
+    // find查找自定义类型数据 需要重载赋值相等运算符
     Person tmp("张三",12);
     vector<Person>::iterator it = find(v.begin(), v.end(), tmp);
     cout<<"查找到的元素:"<<(*it).name<<endl;
     
 }
 
+bool myGrater(int value) {
+    return value > 30;
+}
+
+void test4() {
+    vector<int> v;
+    v.push_back(12);
+    v.push_back(234);
+    v.push_back(4);
+    v.push_back(67);
+    v.push_back(2);
+    v.push_back(432);
+    v.push_back(23);
+    v.push_back(45);
+    v.push_back(6);
+    v.push_back(78);
+    
+    vector<int>::iterator it = find_if(v.begin(), v.end(), myGrater);
+    cout<<"查找到的元素:"<<*it<<endl;
+}
+
 int main(int argc, const char * argv[]) {
     //test1();
     //test2();
-    test3();
+    //test3();
+    test4();
     return 0;
 }
