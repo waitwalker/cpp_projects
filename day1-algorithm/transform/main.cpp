8046387428ec98c59523bcf470da79e4e09f45ea
waitwalker <waitwalker@163.com>
2021-06-18 09:38:35 +0800

    adjacent_find查找相邻重复元素

--- day1-algorithm/transform/main.cpp
+++ day1-algorithm/transform/main.cpp
@@ -118,10 +118,42 @@ void test4() {
     cout<<"查找到的元素:"<<*it1<<endl;
 }
 
+void test5() {
+    vector<int> v;
+    v.push_back(12);
+    v.push_back(234);
+    v.push_back(4);
+    v.push_back(67);
+    v.push_back(2);
+    v.push_back(432);
+    v.push_back(6);
+    v.push_back(23);
+    v.push_back(45);
+    v.push_back(6);
+    
+    v.push_back(78);
+    
+    // 查找相邻重复元素
+    vector<int>::iterator it = adjacent_find(v.begin(), v.end());
+    cout<<"查找到的元素:"<<*it<<endl;
+    
+    vector<Person> v1;
+    v1.push_back(Person("张三", 12));
+    v1.push_back(Person("张1", 13));
+    v1.push_back(Person("张2", 14));
+    v1.push_back(Person("张5", 15));
+    v1.push_back(Person("张5", 15));
+    // find查找自定义类型数据 需要重载赋值相等运算符
+    Person tmp("张三",12);
+    vector<Person>::iterator it1 = adjacent_find(v1.begin(), v1.end());
+    cout<<"查找到的元素:"<<(*it1).name<<endl;
+}
+
 int main(int argc, const char * argv[]) {
     //test1();
     //test2();
     //test3();
-    test4();
+    //test4();
+    test5();
     return 0;
 }
