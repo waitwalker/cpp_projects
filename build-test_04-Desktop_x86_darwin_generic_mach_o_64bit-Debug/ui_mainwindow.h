4e60e405d382d9a4ba3312758ef68074fc83fa42
waitwalker <waitwalker@163.com>
2021-06-22 13:39:12 +0800

    错误提示框

--- build-test_04-Desktop_x86_darwin_generic_mach_o_64bit-Debug/ui_mainwindow.h
+++ build-test_04-Desktop_x86_darwin_generic_mach_o_64bit-Debug/ui_mainwindow.h
@@ -12,6 +12,8 @@
 #include <QtCore/QVariant>
 #include <QtGui/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
@@ -24,11 +26,19 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QAction *actionopen;
     QAction *actionsave;
+    QAction *actionnew1;
+    QAction *actionnew2;
+    QAction *actionnew3;
+    QAction *actionnew4;
+    QAction *actionnew5;
+    QAction *actionnew6;
     QWidget *centralwidget;
+    QFormLayout *formLayout;
+    QLabel *label;
     QMenuBar *menubar;
     QMenu *menu;
+    QMenu *menuopen;
     QMenu *menu_2;
     QStatusBar *statusbar;
     QToolBar *toolBar;
@@ -37,20 +47,42 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(521, 274);
-        actionopen = new QAction(MainWindow);
-        actionopen->setObjectName(QString::fromUtf8("actionopen"));
+        MainWindow->resize(473, 298);
         actionsave = new QAction(MainWindow);
         actionsave->setObjectName(QString::fromUtf8("actionsave"));
+        actionnew1 = new QAction(MainWindow);
+        actionnew1->setObjectName(QString::fromUtf8("actionnew1"));
+        actionnew2 = new QAction(MainWindow);
+        actionnew2->setObjectName(QString::fromUtf8("actionnew2"));
+        actionnew3 = new QAction(MainWindow);
+        actionnew3->setObjectName(QString::fromUtf8("actionnew3"));
+        actionnew4 = new QAction(MainWindow);
+        actionnew4->setObjectName(QString::fromUtf8("actionnew4"));
+        actionnew5 = new QAction(MainWindow);
+        actionnew5->setObjectName(QString::fromUtf8("actionnew5"));
+        actionnew6 = new QAction(MainWindow);
+        actionnew6->setObjectName(QString::fromUtf8("actionnew6"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        formLayout = new QFormLayout(centralwidget);
+        formLayout->setObjectName(QString::fromUtf8("formLayout"));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font;
+        font.setPointSize(22);
+        label->setFont(font);
+
+        formLayout->setWidget(0, QFormLayout::LabelRole, label);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 521, 21));
+        menubar->setGeometry(QRect(0, 0, 473, 21));
         menubar->setNativeMenuBar(false);
         menu = new QMenu(menubar);
         menu->setObjectName(QString::fromUtf8("menu"));
+        menuopen = new QMenu(menu);
+        menuopen->setObjectName(QString::fromUtf8("menuopen"));
         menu_2 = new QMenu(menubar);
         menu_2->setObjectName(QString::fromUtf8("menu_2"));
         MainWindow->setMenuBar(menubar);
@@ -63,10 +95,16 @@ public:
 
         menubar->addAction(menu->menuAction());
         menubar->addAction(menu_2->menuAction());
-        menu->addAction(actionopen);
+        menu->addAction(menuopen->menuAction());
         menu->addAction(actionsave);
         menu->addSeparator();
         menu->addSeparator();
+        menuopen->addAction(actionnew1);
+        menuopen->addAction(actionnew2);
+        menuopen->addAction(actionnew3);
+        menuopen->addAction(actionnew4);
+        menuopen->addAction(actionnew5);
+        menuopen->addAction(actionnew6);
 
         retranslateUi(MainWindow);
 
@@ -76,12 +114,16 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        actionopen->setText(QCoreApplication::translate("MainWindow", "open", nullptr));
-#if QT_CONFIG(shortcut)
-        actionopen->setShortcut(QCoreApplication::translate("MainWindow", "Ctrl+N", nullptr));
-#endif // QT_CONFIG(shortcut)
         actionsave->setText(QCoreApplication::translate("MainWindow", "save", nullptr));
+        actionnew1->setText(QCoreApplication::translate("MainWindow", "new1", nullptr));
+        actionnew2->setText(QCoreApplication::translate("MainWindow", "new2", nullptr));
+        actionnew3->setText(QCoreApplication::translate("MainWindow", "new3", nullptr));
+        actionnew4->setText(QCoreApplication::translate("MainWindow", "new4", nullptr));
+        actionnew5->setText(QCoreApplication::translate("MainWindow", "new5", nullptr));
+        actionnew6->setText(QCoreApplication::translate("MainWindow", "new6", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Label", nullptr));
         menu->setTitle(QCoreApplication::translate("MainWindow", "\346\226\207\344\273\266", nullptr));
+        menuopen->setTitle(QCoreApplication::translate("MainWindow", "open", nullptr));
         menu_2->setTitle(QCoreApplication::translate("MainWindow", "\347\274\226\350\276\221", nullptr));
         toolBar->setWindowTitle(QCoreApplication::translate("MainWindow", "toolBar", nullptr));
     } // retranslateUi
--- test_04/mainwindow.cpp
+++ test_04/mainwindow.cpp
@@ -1,11 +1,51 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDialog>
+#include <QPushButton>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionnew1,&QAction::triggered,[=](){
+        qDebug()<<"new1";
+    });
+
+    this->resize(1200,1200);
+
+    QPushButton *button1 = new QPushButton("弹出",this);
+    button1->setGeometry(200,100,100,100);
+
+    connect(button1,&QPushButton::clicked,[&](){
+        QDialog *dialog = new QDialog(this);
+        dialog->resize(200,100);
+        // 带阻塞的
+        dialog->exec();
+        qDebug()<<"显示模态对话框";
+    });
+
+    QPushButton *button2 = new QPushButton("弹出2",this);
+    button2->setGeometry(200,300,100,100);
+
+    connect(button2,&QPushButton::clicked,[&](){
+        QDialog *dialog = new QDialog(this);
+        dialog->resize(200,100);
+        // 不带阻塞的
+        //dialog->exec();
+        dialog->show();
+        qDebug()<<"显示模态对话框";
+    });
+
+    QPushButton *button3 = new QPushButton("打开",this);
+    button3->setGeometry(200,400,100,100);
+    connect(button3,&QPushButton::clicked,[&](){
+        // 消息对话框 是一个模态的对话框
+        QMessageBox::critical(this,"错误","很严重的错误,留意");
+    });
+
+
 }
 
 MainWindow::~MainWindow()
