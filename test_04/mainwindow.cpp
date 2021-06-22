38fe6cbb2ba9e821f8ba8da0b03d6cfe815647fb
waitwalker <waitwalker@163.com>
2021-06-22 13:48:01 +0800

    询问对话框上的按钮

--- test_04/mainwindow.cpp
+++ test_04/mainwindow.cpp
@@ -27,7 +27,7 @@ MainWindow::MainWindow(QWidget *parent)
     });
 
     QPushButton *button2 = new QPushButton("非模态",this);
-    button2->setGeometry(200,300,100,100);
+    button2->setGeometry(200,200,100,100);
 
     connect(button2,&QPushButton::clicked,[&](){
         QDialog *dialog = new QDialog(this);
@@ -39,7 +39,7 @@ MainWindow::MainWindow(QWidget *parent)
     });
 
     QPushButton *button3 = new QPushButton("错误",this);
-    button3->setGeometry(200,400,100,100);
+    button3->setGeometry(200,300,100,100);
     connect(button3,&QPushButton::clicked,[&](){
         // 错误对话框 是一个模态的对话框
         QMessageBox::critical(this,"错误","很严重的错误,留意");
@@ -47,17 +47,17 @@ MainWindow::MainWindow(QWidget *parent)
 
 
     QPushButton *button4 = new QPushButton("信息",this);
-    button4->setGeometry(200,500,100,100);
+    button4->setGeometry(200,400,100,100);
     connect(button4,&QPushButton::clicked,[&](){
         // 信息对话框 是一个模态的对话框
         QMessageBox::information(this,"信息提示","重要信息提示");
     });
 
     QPushButton *button5 = new QPushButton("询问",this);
-    button5->setGeometry(200,600,100,100);
+    button5->setGeometry(200,500,100,100);
     connect(button5,&QPushButton::clicked,[&](){
         // 询问对话框 是一个模态的对话框
-        QMessageBox::question(this,"询问","请立即续费");
+        QMessageBox::question(this,"询问","请立即续费",QMessageBox::Save | QMessageBox::Cancel);
     });
 
 
