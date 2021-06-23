50d26c15a9b51c4aab6ddd47b9bd91ac0f7cd506
waitwalker <waitwalker@163.com>
2021-06-23 14:28:03 +0800

    Combox 添加item

--- test_06/mainwindow.cpp
+++ test_06/mainwindow.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QComboBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QStringList list;
+    list.append("宝马");
+    list.append("奔驰");
+    list.append("特斯拉");
+    list.append("奥迪");
+    list.append("布加迪");
+    list.append("五菱宏光");
+
+    ui->comboBox->addItems(list);
+
 }
 
 MainWindow::~MainWindow()
