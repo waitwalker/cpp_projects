6e83e4727a876c11f0d8fee839fae2d4466cce9a
waitwalker <waitwalker@163.com>
2021-06-23 08:50:18 +0800

    组容器&单选框

--- build-test_05-Desktop_x86_darwin_generic_mach_o_64bit-Debug/ui_mainwindow.h
+++ build-test_05-Desktop_x86_darwin_generic_mach_o_64bit-Debug/ui_mainwindow.h
@@ -11,12 +11,15 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -31,6 +34,15 @@ public:
     QLineEdit *lineEdit_2;
     QPushButton *loginButton;
     QPushButton *cancelButton;
+    QTabWidget *tabWidget;
+    QWidget *tab;
+    QWidget *tab_2;
+    QGroupBox *groupBox;
+    QRadioButton *radioButton_2;
+    QRadioButton *radioButton;
+    QGroupBox *groupBox_2;
+    QRadioButton *radioButton_4;
+    QRadioButton *radioButton_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -62,6 +74,33 @@ public:
         cancelButton = new QPushButton(centralwidget);
         cancelButton->setObjectName(QString::fromUtf8("cancelButton"));
         cancelButton->setGeometry(QRect(280, 186, 80, 24));
+        tabWidget = new QTabWidget(centralwidget);
+        tabWidget->setObjectName(QString::fromUtf8("tabWidget"));
+        tabWidget->setGeometry(QRect(480, 40, 127, 80));
+        tab = new QWidget();
+        tab->setObjectName(QString::fromUtf8("tab"));
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName(QString::fromUtf8("tab_2"));
+        tabWidget->addTab(tab_2, QString());
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(310, 310, 120, 80));
+        radioButton_2 = new QRadioButton(groupBox);
+        radioButton_2->setObjectName(QString::fromUtf8("radioButton_2"));
+        radioButton_2->setGeometry(QRect(0, 50, 99, 22));
+        radioButton = new QRadioButton(groupBox);
+        radioButton->setObjectName(QString::fromUtf8("radioButton"));
+        radioButton->setGeometry(QRect(0, 20, 99, 22));
+        groupBox_2 = new QGroupBox(centralwidget);
+        groupBox_2->setObjectName(QString::fromUtf8("groupBox_2"));
+        groupBox_2->setGeometry(QRect(140, 320, 120, 80));
+        radioButton_4 = new QRadioButton(groupBox_2);
+        radioButton_4->setObjectName(QString::fromUtf8("radioButton_4"));
+        radioButton_4->setGeometry(QRect(10, 50, 99, 22));
+        radioButton_3 = new QRadioButton(groupBox_2);
+        radioButton_3->setObjectName(QString::fromUtf8("radioButton_3"));
+        radioButton_3->setGeometry(QRect(10, 20, 99, 22));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
@@ -72,6 +111,9 @@ public:
         MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
+
+        tabWidget->setCurrentIndex(0);
+
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
@@ -88,6 +130,14 @@ public:
         lineEdit_2->setPlaceholderText(QCoreApplication::translate("MainWindow", "\350\276\223\345\205\245\345\257\206\347\240\201", nullptr));
         loginButton->setText(QCoreApplication::translate("MainWindow", "\347\231\273\345\275\225", nullptr));
         cancelButton->setText(QCoreApplication::translate("MainWindow", "\345\217\226\346\266\210", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("MainWindow", "Tab 1", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("MainWindow", "Tab 2", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "\346\200\247\345\210\253", nullptr));
+        radioButton_2->setText(QCoreApplication::translate("MainWindow", "\345\245\263", nullptr));
+        radioButton->setText(QCoreApplication::translate("MainWindow", "\347\224\267", nullptr));
+        groupBox_2->setTitle(QCoreApplication::translate("MainWindow", "\345\251\232\345\247\273\347\212\266\345\206\265", nullptr));
+        radioButton_4->setText(QCoreApplication::translate("MainWindow", "\346\234\252\345\251\232", nullptr));
+        radioButton_3->setText(QCoreApplication::translate("MainWindow", " \345\267\262\345\251\232", nullptr));
     } // retranslateUi
 
 };
