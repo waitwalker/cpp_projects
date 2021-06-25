860225028d91569c184f98c3702c4f73824d3c75
waitwalker <waitwalker@163.com>
2021-06-25 10:12:54 +0800

    QImage绘制设备上画圆

--- test_12/mainwindow.cpp
+++ test_12/mainwindow.cpp
@@ -9,13 +9,22 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     this->resize(800,600);
+    // 定义一个QImage图片
+    QImage img;
+    img.load(":/images/123.png");
 
-    // 定义QBitMap 一会绘图设备
-    QBitmap bitMap(200,200);
-    // 定义个画笔
-    QPainter painter(&bitMap);
-    painter.drawEllipse(50,50,50,50);
-    bitMap.save("/Users/waitwalker/Desktop/工作/github_projects/cpp_projects/test_12/bit1.jpg");
+    // 定义一个画笔
+    QPainter painter(&img);
+    painter.drawEllipse(QPoint(100,100),100,100);
+    img.save("/Users/waitwalker/Desktop/工作/github_projects/cpp_projects/test_12/img1.jpg");
+
+
+//    // 定义QBitMap 一会绘图设备
+//    QBitmap bitMap(200,200);
+//    // 定义个画笔
+//    QPainter painter(&bitMap);
+//    painter.drawEllipse(50,50,50,50);
+//    bitMap.save("/Users/waitwalker/Desktop/工作/github_projects/cpp_projects/test_12/bit1.jpg");
 }
 
 MainWindow::~MainWindow()
